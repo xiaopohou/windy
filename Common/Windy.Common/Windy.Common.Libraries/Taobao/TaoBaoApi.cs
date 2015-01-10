8a0e84e3ca5b680bd42f6f8560061ee86cc5c8b7
zuifengke <yehui1989@qq.com>
2015-01-10 22:30:24 +0800

    提交Common
    增加Common类库

---
+++ Common/Windy.Common/Windy.Common.Libraries/Taobao/TaoBaoApi.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography;
+using System.Xml;
+using System.Xml.Linq;
+using System.Web;
+
+
+namespace Windy.Common.Libraries.Taobao
+{
+    public class taobaoApi
+    {
+        public const string VERSION = "1.0";
+
+        private string _sessionID;
+
+        #region 构造函数
+        public taobaoApi()
+        {
+        }
+        public taobaoApi(string sessionID)
+        {
+            _sessionID = sessionID;
+        }
+        #endregion
+
+        #region 属性
+        //获取session
+        public string SessionID
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(HttpContext.Current.Session["sip_sessionid"].ToString()))
+                {
+                    HttpContext.Current.Session["sip_sessionid"] = Guid.NewGuid().ToString();
+                }
+                return HttpContext.Current.Session["sip_sessionid"].ToString();
+            }
+        }
+
+        #endregion
+
+        #region 商品接口
+
+
+
+
+        /// <summary>
+        /// 获取某会员商品列表达式
+        /// </summary>
+        /// <param name="q"></param>
+        /// <param name="fields"></param>
+        /// <param name="page_no"></param>
+        /// <param name="page_size"></param>
+        /// <param name="nicks"></param>
+        /// <returns></returns>
+        public XmlDocument taobao_items_get(string q, string fields, int page_no, int page_size, string nicks)
+        {
+
+            Util.ParamsBuild pb = new Util.ParamsBuild(SessionID, "taobao.items.get");
+
+            pb.AddParam("fields", fields);//"iid,delist_time"
+            pb.AddParam("v", VERSION);
+            if (q != string.Empty)
+                pb.AddParam("q", q);
+            if (page_no != 0)
+                pb.AddParam("page_no", page_no);
+            if (page_size != 0)
+                pb.AddParam("page_size", page_size);
+            // / 
+            pb.AddParam("nicks", nicks);
+            //if(order_by != "")
+            //    pb.AddParam("order_by", order_by);
+            string data = pb.GetURL();
+
+            return Util.HttpRequest(data);
+        }
+
+        /// <summary>
+        /// 获取宝贝列表的返回值
+        /// </summary>
+        /// <param name="q"></param>
+        /// <param name="fields"></param>
+        /// <param name="page_no"></param>
+        /// <param name="page_size"></param>
+        /// <param name="nicks"></param>
+        /// <returns></returns>
+        public string taobao_items_get2(string q, string fields, int page_no, int page_size, string nicks)
+        {
+
+            Util.ParamsBuild pb = new Util.ParamsBuild(SessionID, "taobao.items.get");
+
+            pb.AddParam("fields", fields);//"iid,delist_time"
+            pb.AddParam("v", VERSION);
+            if (q != string.Empty)
+                pb.AddParam("q", q);
+            if (page_no != 0)
+                pb.AddParam("page_no", page_no);
+            if (page_size != 0)
+                pb.AddParam("page_size", page_size);
+            // / 
+            pb.AddParam("nicks", nicks);
+            //if(order_by != "")
+            //    pb.AddParam("order_by", order_by);
+            string data = pb.GetURL();
+
+            return Util.HttpRequest2(data);
+        }
+
+
+
+
+        /// <summary>
+        ///获取当前会话用户（必须为卖家）的出售中商品列表。支持分页。
+        /// </summary>
+        /// <param name="q">搜索字段</param>
+        /// <param name="fields">商品对象字段</param>
+        /// <param name="page_no">页码</param>
+        /// <param name="page_size">页显示数量</param>
+        /// <param name="has_discount">是否参与会员折扣</param>
+        /// <param name="has_showcase">是否橱窗推荐</param>
+        /// <returns></returns>
+        public HttpWebResponse taobao_items_onsale_get(string q, string fields, int page_no, int page_size, bool has_discount, bool has_showcase, string orderby, string cid, string seller_cids)
+        {
+
+            Util.ParamsBuild pb = new Util.ParamsBuild(SessionID, "taobao.items.onsale.get");
+
+            pb.AddParam("fields", fields);//"iid,delist_time"
+            pb.AddParam("v", VERSION);
+            if (q != string.Empty)
+                pb.AddParam("q", q);
+            if (page_no != 0)
+                pb.AddParam("page_no", page_no);
+            if (page_size != 0)
+                pb.AddParam("page_size", page_size);
+            if (has_discount)
+            {
+                pb.AddParam("has_discount", "true");
+            }
+            if (has_showcase)
+            {
+                pb.AddParam("has_showcase", "true");
+            }
+            if (orderby.Length != 0)
+                pb.AddParam("order_by", orderby);
+            if (cid.Length != 0)
+                pb.AddParam("cid", cid);
+            if (seller_cids.Length != 0)
+                pb.AddParam("seller_cids", seller_cids);
+            //pb.AddParam("sip_usertoken", token);
+            string data = pb.GetURL();
+            return Util.HttpRequest3(data);
+        }
+
+
+
+
+
+
+
+        #endregion
+
+        #region 用户接口
+
+        #endregion
+
+        #region 店铺接口
+        /// <summary>
+        /// 
+        /// </summary>
+
+        /// <param name="fields"></param>
+        /// <param name="nicks"></param>
+        /// <returns></returns>
+        public XmlDocument taobao_shop_get(string fields, string nicks)
+        {
+
+            Util.ParamsBuild pb = new Util.ParamsBuild(SessionID, "taobao.shop.get");
+
+            pb.AddParam("fields", fields);//nick,sex,buyer_credit,seller_credit,location.city,created,real_name,shop_id
+            pb.AddParam("v", VERSION);
+            pb.AddParam("nick", nicks);
+
+            string data = pb.GetURL();
+
+            return Util.HttpRequest(data);
+        }
+
+
+        //taobao.shop.update
+        public XmlDocument taobao_shop_update(string _title, string _bulletin, string _desc)
+        {
+            Util.ParamsBuild pb = new Util.ParamsBuild(SessionID, "taobao.shop.update");
+
+            pb.AddParam("title", _title);//nick,sex,buyer_credit,seller_credit,location.city,created,real_name,shop_id
+            pb.AddParam("v", VERSION);
+            pb.AddParam("bulletin", _bulletin);
+            pb.AddParam("desc", _desc);
+            string data = pb.GetURL();
+
+            return Util.HttpRequest(data);
+        }
+        #endregion
+
+
+
+        #region 类目属性接口
+
+
+
+        //taobao.itemcats.get.v2 ok
+        /// <summary>
+        /// 获取后台供卖家发布商品的标准商品类目(获取品牌型号)
+
+        /// </summary>
+        /// <param name="parent_cid"></param>
+        /// <param name="cids"></param>
+        /// <param name="fields"></param>
+        /// <returns></returns>
+        public HttpWebResponse taobao_itemcats_get_v2(string parent_cid, string cids, string fields)
+        {
+            Util.ParamsBuild pb = new Util.ParamsBuild(SessionID, "taobao.itemcats.get.v2");
+
+            if (parent_cid.Length != 0 || cids.Length != 0)
+            {
+                if (parent_cid != "")
+                {
+                    pb.AddParam("parent_cid", parent_cid);//
+
+                }
+                if (cids != "")
+                {
+                    pb.AddParam("cids", cids);//
+
+                }
+            }
+            pb.AddParam("fields", fields);
+            pb.AddParam("v", VERSION);
+            string data = pb.GetURL();
+
+            return Util.HttpRequest3(data);
+        }
+
+
+        #endregion
+    }
+}
---
+++ Common/Windy.Common/Windy.Common.Libraries/Taobao/Util.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+namespace Windy.Common.Libraries.Taobao
+{
+    public class Util
+    {
+
+        #region 测试用 返回字符串结果集
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="image"></param>
+        /// <param name="uploadfile"></param>
+        /// <param name="fileFormName"></param>
+        /// <param name="contenttype"></param>
+        /// <returns></returns>
+        public static HttpWebResponse HttpRequest5(string data, byte[] image, string uploadfile, string fileFormName, string contenttype)
+        {
+            ////ASCIIEncoding encoding = new ASCIIEncoding();
+            //byte[] postdata = System.Text.Encoding.UTF8.GetBytes(data);//所有要传参数拼装
+            //// Prepare web request...
+            ////目前阿里软件的服务集成平台（SIP）的接口测试地址是：http://sipdev.alisoft.com/sip/rest，生产环境地址是：http://sip.alisoft.com/sip/rest,
+            ////这里使用测试接口先，到正式上线时需要做切换
+            //string url = System.Configuration.ConfigurationManager.AppSettings["APPURL"];
+            //HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
+            //myRequest.Method = "POST";
+            //myRequest.ContentType = "application/x-www-form-urlencoded";
+            //myRequest.ContentLength = postdata.Length;
+            //Stream newStream = myRequest.GetRequestStream();
+            //// Send the data.
+            //newStream.Write(postdata, 0, postdata.Length);
+            //newStream.Close();
+            //// Get response
+            //HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            //return myResponse;
+
+            string url = System.Configuration.ConfigurationManager.AppSettings["APPURL"];
+
+            Uri uri = new Uri(url + data);
+
+
+            string boundary = "----------" + DateTime.Now.Ticks.ToString("x");
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(uri);
+            webrequest.ContentType = "text/plain";
+            webrequest.Method = "POST";
+
+            // Build up the post message header
+            if (uploadfile != null && uploadfile.Length > 0)
+            {
+                webrequest.ContentType = "multipart/form-data; boundary=" + boundary;
+                StringBuilder sb = new StringBuilder();
+                sb.Append("--");
+                sb.Append(boundary);
+                sb.Append("\r\n");
+                sb.Append("Content-Disposition: form-data; name=\"");
+                sb.Append(fileFormName);
+                sb.Append("\"; filename=\"");
+                sb.Append("test.jpg");
+                sb.Append("\"");
+                sb.Append("\r\n");
+                sb.Append("Content-Type: ");
+                sb.Append(contenttype);
+                sb.Append("\r\n");
+                sb.Append("\r\n");
+
+                string postHeader = sb.ToString();
+                byte[] postHeaderBytes = Encoding.UTF8.GetBytes(postHeader);
+
+                // Build the trailing boundary string as a byte array
+
+                // ensuring the boundary appears on a line by itself
+
+                byte[] boundaryBytes =
+                       Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+
+
+                long length = postHeaderBytes.Length + image.Length +
+                                                       boundaryBytes.Length;
+                webrequest.ContentLength = length;
+
+                Stream requestStream = webrequest.GetRequestStream();
+
+                // Write out our post header
+
+                requestStream.Write(postHeaderBytes, 0, postHeaderBytes.Length);
+
+
+                // Write out the file contents
+                requestStream.Write(image, 0, image.Length);
+
+                // Write out the trailing boundary
+
+                requestStream.Write(boundaryBytes, 0, boundaryBytes.Length);
+            }
+            HttpWebResponse responce = (HttpWebResponse)webrequest.GetResponse();
+            //Stream s = responce.GetResponseStream();
+            //StreamReader sr = new StreamReader(s);
+
+            //return sr.ReadToEnd();
+            return responce;
+
+        }
+
+        /// 
+        /// <summary>
+        /// 上传图片时用
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static HttpWebResponse HttpRequest4(string data)
+        {
+            //ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] postdata = System.Text.Encoding.UTF8.GetBytes(data);//所有要传参数拼装
+            // Prepare web request...
+            //目前阿里软件的服务集成平台（SIP）的接口测试地址是：http://sipdev.alisoft.com/sip/rest，生产环境地址是：http://sip.alisoft.com/sip/rest,
+            //这里使用测试接口先，到正式上线时需要做切换
+            string url = System.Configuration.ConfigurationManager.AppSettings["APPURL"];
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+            myRequest.ContentLength = postdata.Length;
+            Stream newStream = myRequest.GetRequestStream();
+            // Send the data.
+            newStream.Write(postdata, 0, postdata.Length);
+            newStream.Close();
+            // Get response
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            return myResponse;
+
+
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static HttpWebResponse HttpRequest3(string data)
+        {
+            //ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] postdata = System.Text.Encoding.UTF8.GetBytes(data);//所有要传参数拼装
+            // Prepare web request...
+            //目前阿里软件的服务集成平台（SIP）的接口测试地址是：http://sipdev.alisoft.com/sip/rest，生产环境地址是：http://sip.alisoft.com/sip/rest,
+            //这里使用测试接口先，到正式上线时需要做切换
+            string url = System.Configuration.ConfigurationManager.AppSettings["APPURL"];
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+            myRequest.ContentLength = postdata.Length;
+            Stream newStream = myRequest.GetRequestStream();
+            // Send the data.
+            newStream.Write(postdata, 0, postdata.Length);
+            newStream.Close();
+            // Get response
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+
+            return myResponse;
+
+
+        }
+
+
+        /// <summary>
+        /// 测试用 返回字符串结果集
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string HttpRequest2(string data)
+        {
+            //ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] postdata = System.Text.Encoding.UTF8.GetBytes(data);//所有要传参数拼装
+            // Prepare web request...
+            //目前阿里软件的服务集成平台（SIP）的接口测试地址是：http://sipdev.alisoft.com/sip/rest，生产环境地址是：http://sip.alisoft.com/sip/rest,
+            //这里使用测试接口先，到正式上线时需要做切换
+            string url = System.Configuration.ConfigurationManager.AppSettings["APPURL"];
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+            myRequest.ContentLength = postdata.Length;
+            Stream newStream = myRequest.GetRequestStream();
+            // Send the data.
+            newStream.Write(postdata, 0, postdata.Length);
+            newStream.Close();
+            // Get response
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            StreamReader reader = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string content = reader.ReadToEnd();
+            //XmlDocument xmlDoc = new XmlDocument();
+            //xmlDoc.LoadXml(content);
+            //XmlNode node = xmlDoc.SelectSingleNode("/error_rsp/code");
+            //if (node != null && node.InnerText != string.Empty)
+            //{
+
+            //    throw new ApiException(node.InnerText, xmlDoc.SelectSingleNode("/error_rsp/msg").InnerText);
+            //}
+            return content;
+
+        }
+
+        #endregion
+
+
+        #region 正式用 返回Xml结果集
+
+        public static XmlDocument HttpRequest(string data)
+        {
+            //ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] postdata = System.Text.Encoding.UTF8.GetBytes(data);//所有要传参数拼装
+            // Prepare web request...
+            //目前阿里软件的服务集成平台（SIP）的接口测试地址是：http://sipdev.alisoft.com/sip/rest，生产环境地址是：http://sip.alisoft.com/sip/rest,
+            //这里使用测试接口先，到正式上线时需要做切换
+            string url = System.Configuration.ConfigurationManager.AppSettings["APPURL"];
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(url);
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+            myRequest.ContentLength = postdata.Length;
+            Stream newStream = myRequest.GetRequestStream();
+            // Send the data.
+            newStream.Write(postdata, 0, postdata.Length);
+            newStream.Close();
+            // Get response
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            StreamReader reader = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string content = reader.ReadToEnd();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(content);
+            XmlNode node = xmlDoc.SelectSingleNode("/error_rsp/code");
+            if (node != null && node.InnerText != string.Empty)
+            {
+
+                // throw new ApiException(node.InnerText, xmlDoc.SelectSingleNode("/error_rsp/msg").InnerText);
+            }
+            return xmlDoc;
+
+        }
+
+        #endregion
+
+
+        public static string HttpRequest(string data, string xPath)
+        {
+            XmlDocument doc = HttpRequest(data);
+            return doc.SelectSingleNode(xPath).InnerText;
+        }
+
+
+
+        #region MD5加密
+        /// <summary>
+        /// md5加密
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static string MD5(string data)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            return BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(data))).Replace("-", "");
+        }
+
+
+        #endregion
+
+        #region 参数创建类
+        public class ParamsBuild
+        {
+            string _code { get; set; }
+            SortedList _mySL = new SortedList();
+
+            public ParamsBuild()
+                : this(Util.GetCode)
+            {
+
+            }
+            public ParamsBuild(string code)
+            {
+                _code = code;
+            }
+            public ParamsBuild(System.Web.HttpContext content, string apiName)
+                : this(content.Session.SessionID, apiName)
+            {
+
+            }
+            public ParamsBuild(string sip_sessionid, string apiName)
+                : this(Util.GetCode)
+            {
+                AddParam("sip_appkey", Util.GetAppID);
+                AddParam("sip_apiname", apiName);
+                AddParam("sip_timestamp", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                AddParam("sip_sessionid", sip_sessionid);
+            }
+            public void AddParam(string name, string value)
+            {
+                _mySL.Add(name, value);
+            }
+            public void AddParam(string name, int value)
+            {
+                _mySL.Add(name, value);
+            }
+            public string GetURL()
+            {
+                StringBuilder orgin = new StringBuilder();
+                orgin.Append(_code); //将安全编码放字符串首位
+                //对list里的参数进行拼装，参数名+参数值，按自然排序，即所有参数字母排序
+                StringBuilder _url = new StringBuilder();
+                foreach (DictionaryEntry Item in _mySL)
+                {
+                    // ListItem newListItem = new ListItem();
+                    orgin.Append(Item.Key.ToString());
+                    if (Item.Value != null)
+                    {
+                        orgin.Append(Item.Value.ToString());
+                    }
+                    _url.AppendFormat("{0}={1}&", Item.Key, Item.Value);
+
+                }
+                _url.AppendFormat("sip_sign={0}", Util.MD5(orgin.ToString()));
+                return _url.ToString();
+            }
+
+
+            /// <summary>
+            /// 获取签名
+            /// </summary>
+            /// <returns></returns>
+            public string GetSign()
+            {
+                StringBuilder orgin = new StringBuilder();
+                orgin.Append(Util.GetCode); //将安全编码放字符串首位
+                //对list里的参数进行拼装，参数名+参数值，按自然排序，即所有参数字母排序
+                StringBuilder _url = new StringBuilder();
+                foreach (DictionaryEntry Item in _mySL)
+                {
+                    // ListItem newListItem = new ListItem();
+                    orgin.Append(Item.Key.ToString());
+                    if (Item.Value != null)
+                    {
+                        orgin.Append(Item.Value.ToString());
+                    }
+                }
+                return Util.MD5(orgin.ToString());
+
+            }
+        }
+
+        #endregion
+
+        #region 获取APPID
+        public static string GetAppID
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["AppID"];
+            }
+        }
+
+        #endregion
+
+        #region 获取APPcode
+
+        public static string GetCode
+        {
+            get
+            {
+                //软件注册时获得
+                return System.Configuration.ConfigurationManager.AppSettings["AppCode"];
+            }
+        }
+
+        #endregion
+
+        #region 根据软件互联平台的用户ID，获取用户的阿里巴巴中文站（或淘宝）帐号。
+
+        /// <summary>
+        /// 根据软件互联平台的用户ID，获取用户的阿里巴巴中文站（或淘宝）帐号。
+        /// </summary>
+        /// <param name="appId"></param>
+        /// <param name="appUserId"></param>
+        /// <returns></returns>
+        public static string getDomainLoginId(string appId, string appUserId)
+        {
+            string code = GetCode;
+            ParamsBuild pb = new ParamsBuild(code);
+            pb.AddParam("sip_appkey", appId);
+            pb.AddParam("sip_apiname", "alisoft.udb.getDomainLoginId");
+            pb.AddParam("sip_timestamp", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            pb.AddParam("domainid", "1");
+            pb.AddParam("userid", appUserId);
+            string data = pb.GetURL();
+
+
+
+            //解析接口返回值，这里选用XML格式的解析，接口默认返回是XML格式
+            XmlDocument xmlDoc = Util.HttpRequest(data);
+
+            XmlNode xn = xmlDoc.SelectSingleNode("StringResult/code");
+            XmlNode xn2 = xmlDoc.SelectSingleNode("StringResult/result");
+            if (xn.InnerText != "0")
+            {
+                return appUserId;
+            }
+            else
+            {
+                return xn2.InnerText;
+            }
+        }
+        #endregion
+    }
+}
