96229395b722d355a9202ba030f90f555e5efba4
zuifengke <yehui1989@qq.com>
2015-01-12 16:00:20 +0800

    微信开发发送消息接口调用

--- WebMVC/WebMVC/Areas/weixin/Controllers/HomeController.cs
+++ WebMVC/WebMVC/Areas/weixin/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Senparc.Weixin.MP.CommonAPIs;
+﻿using Senparc.Weixin.Exceptions;
+using Senparc.Weixin.MP.AdvancedAPIs;
+using Senparc.Weixin.MP.CommonAPIs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,44 @@ namespace Windy.WebMVC.Areas.weixin.Controllers
 
             return View(result);
         }
+        public ActionResult UserInfo()
+        {
+            WeiXinAppInfo weiXinAppInfo = ServerParam.Instance.WeiXinAppInfo;
+            AccessTokenContainer.Register(ServerParam.Instance.WeiXinAppInfo.AppID, ServerParam.Instance.WeiXinAppInfo.AppSecret);
+            var accessToken = AccessTokenContainer.GetToken(ServerParam.Instance.WeiXinAppInfo.AppID);
+
+            var result = Senparc.Weixin.MP.AdvancedAPIs.User.Info(accessToken, ServerParam.Instance.WeiXinAppInfo.OpenID);
+
+            return View(result);
+        }
+
+        public ActionResult SendText()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendText(FormCollection collection)
+        {
+            string text = collection["text"];
+            Response.Write("你发送消息" + text);
+            AccessTokenContainer.Register(ServerParam.Instance.WeiXinAppInfo.AppID, ServerParam.Instance.WeiXinAppInfo.AppSecret);
+            var accessToken = AccessTokenContainer.GetToken(ServerParam.Instance.WeiXinAppInfo.AppID);
+            try
+            {
+                var result = Custom.SendText(accessToken, ServerParam.Instance.WeiXinAppInfo.OpenID,text);
+               
+            }
+            catch (ErrorJsonResultException ex)
+            {
+                Response.Write("发送失败");
+
+                return View();
+            }
+
+            Response.Write("发送成功");
+            return View();
+        }
+        
 
     }
 }
